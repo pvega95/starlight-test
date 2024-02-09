266f9acec102ac9520bff7ae0bf86f34e696edea
Paul Vega <paul.vega@tranzact.net>
2024-02-09 15:27:35 -0500

    update

---
+++ src/main-cuy.tsx
@@ -0,0 +1,21 @@
+import { StyledEngineProvider } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import theme from "./theme/theme";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
+
+export { init };
+
+function init() {
+  ReactDOM.createRoot(document.getElementById("moonlight-root") as HTMLElement).render(
+    <React.StrictMode>
+      <StyledEngineProvider injectFirst>
+        <StyledThemeProvider theme={theme("light")}>
+            <App />
+        </StyledThemeProvider>
+      </StyledEngineProvider>
+    </React.StrictMode>
+  );
+}
